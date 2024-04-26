e8facc60e1ea01791bdd7def61d18c933a49e1a7
Vidit <viditviionn1@gmail.com>
2024-04-26 14:12:34 +0530

    sajkndjs

--- functions/api.js
+++ functions/api.js
@@ -2,15 +2,18 @@ const express = require('express');
 const serverless = require('serverless-http');
 const dotenv = require('dotenv');
 
-const app = express();
-const router = express.Router();
 
 const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
 dotenv.config({ path: envFile });
-console.log(process.env.NODE_ENV)
+
+
+const app = express();
+const router = express.Router();
+
+
 
 router.get('/', (req, res) => {
-    res.send('Hello World from development pr  Netlify Lambda!');
+    res.send('Hell ooobhaiiiii jayesh Lambda!');
 });
 
 app.use('/.netlify/functions/api', router); // path must route to lambda
